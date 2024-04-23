21e630c7e62c8c18ea3ca6c01c03a4d95c9aa9c4
toni-umbrella <166714192+toni-umbrella@users.noreply.github.com>
2024-04-23 15:33:42 +0200

    Update ServiceProvider.php

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace ShippingServiceProviderTemplate\Providers;
+namespace ShippingServiceProviderTemplateRitter\Providers;
 
 use Plenty\Plugin\ServiceProvider as PlentyServiceProvider;
 use Plenty\Modules\Order\Shipping\ServiceProvider\Services\ShippingServiceProviderService;
-use ShippingServiceProviderTemplate\Helpers\ShippingServiceProvider;
+use ShippingServiceProviderTemplateRitter\Helpers\ShippingServiceProvider;
 
 /**
  * Class ServiceProvider
- * @package ShippingServiceProviderTemplate\Providers
+ * @package ShippingServiceProviderTemplateRitter\Providers
  */
 class ServiceProvider extends PlentyServiceProvider
 {
@@ -33,7 +33,7 @@ class ServiceProvider extends PlentyServiceProvider
             ShippingServiceProvider::PLUGIN_NAME,
             ['de' => ShippingServiceProvider::SHIPPING_SERVICE_PROVIDER_NAME, 'en' => ShippingServiceProvider::SHIPPING_SERVICE_PROVIDER_NAME],
             [
-                'ShippingServiceProviderTemplate\\Controllers\\ShipmentController@getLabels',
+                'ShippingServiceProviderTemplateRitter\\Controllers\\ShipmentController@getLabels',
             ]
         );
     }
